1b6307066c51ddd10afc93f8f85f4d10287e9cbf
tujhrf100209-sketch <tujhrf100209@gmail.com>
2026-09-16 20:15:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
+<?php
 
+$a = 5;
+
+echo $a . "<br>";
+
+echo -$a . "<br>";
+
+$a = $a + 3;
+echo $a . "<br>";
+
+$a = $a - 5;
+echo $a . "<br>";
+
+$a = $a * 2;
+echo $a . "<br>";
+
+$a = $a / 5;
+echo $a . "<br>";
+
+echo $a % 2 . "<br>";
+
+echo $a ** 3 . "<br>";
+
+echo "Wartość zmiennej \$a wynosi: " . $a . " i już się nie zmieni.";
+
+?>
